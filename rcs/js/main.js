a390c85ac58bdd2d8aaf997e6b296561fc434ce9
BigBadBubbaGump <32795230+BigBadBubbaGump@users.noreply.github.com>
2017-12-10 14:31:17 -0500

    Buttons.

--- rcs/js/main.js
+++ rcs/js/main.js
@@ -1,36 +1,36 @@
 
 $(document).ready(function(){
     $("#flip1").click(function(){
-        $("#panel1").slideToggle("fast");
+        $("#panel1").fadeToggle()("fast");
             });
 });
 
 $(document).ready(function(){
     $("#flip2").click(function(){
-        $("#panel2").slideToggle("fast");
+        $("#panel2").slideToggle()("fast");
             });
 });
 
 $(document).ready(function(){
     $("#flip3").click(function(){
-        $("#panel3").slideToggle("fast");
+        $("#panel3").slideToggle()("fast");
             });
 });
 
 $(document).ready(function(){
     $("#flip4").click(function(){
-        $("#panel4").slideToggle("fast");
+        $("#panel4").slideToggle()("fast");
             });
 });
 
 $(document).ready(function(){
     $("#flip5").click(function(){
-        $("#panel5").slideToggle("fast");
+        $("#panel5").slideToggle()("fast");
             });
 });
 
 $(document).ready(function(){
     $("#flip6").click(function(){
-        $("#panel6").slideToggle("fast");
+        $("#panel6").slideToggle()("fast");
             });
 });
