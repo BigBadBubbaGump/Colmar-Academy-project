2e09ccf68566b790df6cbbb5d5b16f3db9de5c90
BigBadBubbaGump <32795230+BigBadBubbaGump@users.noreply.github.com>
2017-12-10 08:44:26 -0500

    add toggled mobile courses.

---
+++ rcs/js/main.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panel1").slideToggle("fast");
+            });
+});
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+        $("#panel2").slideToggle("fast");
+            });
+});
+
+$(document).ready(function(){
+    $("#flip3").click(function(){
+        $("#panel3").slideToggle("fast");
+            });
+});
+
+$(document).ready(function(){
+    $("#flip4").click(function(){
+        $("#panel4").slideToggle("fast");
+            });
+});
+
+$(document).ready(function(){
+    $("#flip5").click(function(){
+        $("#panel5").slideToggle("fast");
+            });
+});
+
+$(document).ready(function(){
+    $("#flip6").click(function(){
+        $("#panel6").slideToggle("fast");
+            });
+});
